cd3da93e17d85b1dcbc587bb96e38c62a29c3cad
Bobby <bobur0218programmer@gmail.com>
2024-07-06 15:30:55 +0500

    start

--- client/src/layouts/MainLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function MainLayout({ children }: { children: ReactNode }) {
-    return (
-        <div className="font-montserrat relative flex flex-col h-screen">
-            {children}
-        </div>
-    );
-}
--- client/src/layouts/PageLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function PageLayout({ children }: { children: ReactNode }) {
-    return (
-        <div className="px-20 py-5 w-full h-auto">
-            {children}
-        </div>
-    );
-}
