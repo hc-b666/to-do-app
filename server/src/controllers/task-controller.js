edf93e8174d61d5c5fb18a356abd2945f12530e5
Bobby <bobur0218programmer@gmail.com>
2024-07-09 19:12:14 +0500

    backend/ api/ getTodayTasksLength, updateTask, deleteTask

--- server/src/controllers/task-controller.js
+++ server/src/controllers/task-controller.js
@@ -57,6 +57,24 @@ exports.getTasks = async (req, res) => {
   }
 };
 
+exports.getTodayTasksLength = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const getTodayTasksLengthSql = `SELECT COUNT(*) AS taskCount FROM tasks WHERE user_id = ? AND DATE(deadline) = DATE('now') AND status IN ('to do', 'doing')`;
+
+    try {
+      const [result] = await query(getTodayTasksLengthSql, [userId]);
+
+      return res.status(200).json({ length: result.taskCount, status: 200 });
+    } catch (dberr) {
+      console.error('Database error:', dberr);
+      return res.status(500).json({ error: 'Database error', status: 500 }); 
+    }   
+  } catch (error) {
+    return res.status(500).json({ error: 'An unknown error occurred', status: 500 });
+  }
+};
+
 exports.postTask = async (req, res) => {
   try {
     const userId = req.user.userId;
@@ -80,11 +98,16 @@ exports.postTask = async (req, res) => {
 exports.updateTaskStatus = async (req, res) => {
   try {
     const userId = req.user.userId;
-    const { status } = req.body;
     const taskId = req.params.taskId;
-
-    if (!taskId || !status) {
+    
+    if (!taskId) {
       return res.status(400).json({ error: 'No task id was provided', status: 400 });
+    }
+
+    const { status } = req.body;
+
+    if (!status) {
+      return res.status(400).json({ error: 'Bad request', status: 400 });
     }
 
     const updateTaskStatusSql = `UPDATE tasks SET status = ? WHERE id = ? AND user_id = ?`;
@@ -99,6 +122,63 @@ exports.updateTaskStatus = async (req, res) => {
     }
 
   } catch (err) {
+    return res.status(500).json({ error: 'An unknown error occured', status: 500 });
+  }
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const taskId = req.params.taskId;
+
+    if (!taskId) {
+      return res.status(400).json({ error: 'No task id was provided', status: 400 });
+    }
+
+    const { title, description, deadline, status } = req.body;
+
+    if (!title || !description || !deadline || !status) {
+      return res.status(400).json({ error: 'Bad request', status: 400 });
+    }
+
+    const updateTaskSql = `UPDATE tasks SET title = ?, description = ?, deadline = ?, status = ? WHERE id = ? AND user_id = ?`;
+
+    try {
+      await query(updateTaskSql, [title, description, deadline, status, taskId, userId]);
+
+      return res.status(201).json({ message: 'Task updated successfully', status: 201 });
+    } catch (dberr) {
+      console.error('Error with updating the task:', dberr);
+      return res.status(500).json({ error: 'Database error', status: 500 });
+    }
+
+  } catch (error) {
+    return res.status(500).json({ error: 'An unknown error occured', status: 500 });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    const userId = req.user.userId;
+    const taskId = req.params.taskId;
+
+    if (!taskId) {
+      return res.status(400).json({ error: 'No task id was provided', status: 400 });
+    }
+
+    const deleteTaskSql = `DELETE FROM tasks WHERE id = ? AND user_id = ?`; 
+
+    try {
+      await query(deleteTaskSql, [taskId, userId]);
+
+      return res.status(201).json({ message: 'Task deleted successfully', status: 201 });
+    } catch (dberr) {
+      console.error('Error with deleting the task:', dberr);
+      return res.status(500).json({ error: 'Database error', status: 500 });
+    }
+
+
+  } catch (error) {
     return res.status(500).json({ error: 'An unknown error occured', status: 500 });
   }
 };
--- server/src/routes/task-routes.js
+++ server/src/routes/task-routes.js
@@ -11,7 +11,10 @@ const taskController = require('../controllers/task-controller');
 router.get('/getStatuses', authMiddleware, taskController.getStatuses);
 router.post('/postStatus', authMiddleware, taskController.postStatus);
 router.get('/getTasks', authMiddleware, taskController.getTasks);
+router.get('/getTodayTasksLength', authMiddleware, taskController.getTodayTasksLength);
 router.post('/postTask', authMiddleware, taskController.postTask);
 router.put('/updateTaskStatus/:taskId', authMiddleware, taskController.updateTaskStatus);
+router.put('/updateTask/:taskId', authMiddleware, taskController.updateTask);
+router.delete('/deleteTask/:taskId', authMiddleware, taskController.deleteTask);
 
 module.exports = router;
